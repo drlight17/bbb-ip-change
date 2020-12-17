95e3a811e7db065776700ac9e0354af9e0f091fa
drlight17 <37434652+drlight17@users.noreply.github.com>
2020-12-17 18:24:31 +0300

    Update bbb-reconfigure.sh
    added new config file
    cleaned up

--- bbb-reconfigure.sh
+++ bbb-reconfigure.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
+# drlight 17.12.2020 Script to change config of BBB with new ip address depend on current ISP
 
-#named_dir="/etc/namedb/master"
 current_isp=`cat /usr/scripts/ch-gateway/current`
-#named_dir="/"
-#named_tpl="/etc/namedb/template"
 vars_xml_templ="/usr/scripts/bbb/vars.xml.templ"
 vars_xml="/opt/freeswitch/etc/freeswitch/vars.xml"
 external_xml_templ="/usr/scripts/bbb/external.xml.templ"
@@ -12,35 +10,35 @@ sip_nginx_templ="/usr/scripts/bbb/sip.nginx.templ"
 sip_nginx="/etc/bigbluebutton/nginx/sip.nginx"
 default_yml_templ="/usr/scripts/bbb/default.yml.templ"
 default_yml="/usr/local/bigbluebutton/bbb-webrtc-sfu/config/default.yml"
-#serial_new=`date "+%y%m%d%H%M"`
+default_bbb_sip_templ="/usr/scripts/bbb/bigbluebutton-sip.properties.templ"
+default_bbb_sip="/usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties"
 cur_date=`date --date="+5 minutes" --rfc-3339=seconds`
-relant_ip="91.210.207.6"
-elvis_ip="195.88.23.216"
+ISP1_ip="xxx.xxx.xxx.xxx"
+ISP2_ip="yyy.yyy.yyy.yyy"
 
-
-if [ "x$current_isp" = "xrelant" ]; then
+if [ "x$current_isp" = "xisp1" ]; then
     #echo ${serial_new};
     #killall named
 
-    cat ${vars_xml_templ} | sed "s/_iptempl_/${relant_ip}/" >${vars_xml};
-    cat ${external_xml_templ} | sed "s/_iptempl_/${relant_ip}/" >${external_xml};
-    cat ${sip_nginx_templ} | sed "s/_iptempl_/${relant_ip}/" >${sip_nginx};
-    cat ${default_yml_templ} | sed "s/_iptempl_/${relant_ip}/" >${default_yml};
-    echo ${cur_date}" BBB configs have been updated to Relant ext IP!" >> /var/log/bbb_config_update.log;
-    #/usr/sbin/rndc reload;
+    cat ${vars_xml_templ} | sed "s/_iptempl_/${ISP1_ip}/" >${vars_xml};
+    cat ${external_xml_templ} | sed "s/_iptempl_/${ISP1_ip}/" >${external_xml};
+    cat ${sip_nginx_templ} | sed "s/_iptempl_/${ISP1_ip}/" >${sip_nginx};
+    cat ${default_yml_templ} | sed "s/_iptempl_/${ISP1_ip}/" >${default_yml};
+    cat ${default_bbb_sip_templ} | sed "s/_iptempl_/${ISP1_ip}/" >${default_bbb_sip};
+    echo ${cur_date}" BBB configs have been updated to ISP1 ext IP!" >> /var/log/bbb_config_update.log;
 else
-    if [ "x$current_isp" = "xelvis" ]; then
+    if [ "x$current_isp" = "xisp2" ]; then
     #echo ${serial_new};
     #killall named
-    cat ${vars_xml_templ} | sed "s/_iptempl_/${elvis_ip}/" >${vars_xml};
-    cat ${external_xml_templ} | sed "s/_iptempl_/${elvis_ip}/" >${external_xml};
-    cat ${sip_nginx_templ} | sed "s/_iptempl_/${elvis_ip}/" >${sip_nginx};
-    cat ${default_yml_templ} | sed "s/_iptempl_/${elvis_ip}/" >${default_yml};
-    #/usr/sbin/rndc reload;
-    echo ${cur_date}" BBB configs have been updated to Elvis ext IP!" >> /var/log/bbb_config_update.log;
+    cat ${vars_xml_templ} | sed "s/_iptempl_/${ISP2_ip}/" >${vars_xml};
+    cat ${external_xml_templ} | sed "s/_iptempl_/${ISP2_ip}/" >${external_xml};
+    cat ${sip_nginx_templ} | sed "s/_iptempl_/${ISP2_ip}/" >${sip_nginx};
+    cat ${default_yml_templ} | sed "s/_iptempl_/${ISP2_ip}/" >${default_yml};
+    cat ${default_bbb_sip_templ} | sed "s/_iptempl_/${ISP2_ip}/" >${default_bbb_sip};
+    echo ${cur_date}" BBB configs have been updated to ISP2 ext IP!" >> /var/log/bbb_config_update.log;
     fi
 fi
 
 bbb-conf --restart
 service freeswitch restart
-echo ${cur_date}" BBB and FreeSwitch have been restarted!" >> /var/log/bbb_config_update.log;
+echo ${cur_date}" BBB and FreeSwitch have been restarted!" >> /var/log/bbb_config_update.log; 
